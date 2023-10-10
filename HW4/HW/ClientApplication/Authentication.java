d5a375a7e231a3aba36bde4bb37cf139aa255fe2
Vitaly Lukyanov <mszvibel@gmail.com>
2023-10-10 14:48:50 +0300

    Add HW4

---
+++ HW4/HW/ClientApplication/Authentication.java
@@ -0,0 +1,26 @@
+package HW.HW4.HW.ClientApplication;
+
+import HW.HW4.HW.Core.UserProvider;
+import HW.HW4.HW.Models.User;
+
+/**
+ * Класс аутентификации пользователя
+ */
+public class Authentication {
+    /**
+     * Метод производит аутентификацию
+     *
+     * @param userProvider
+     * @param login
+     * @param passHash
+     * @return
+     */
+    public static User authentication(UserProvider userProvider, String login, int passHash) {
+        var client = userProvider.getClientByName(login);
+        if (client.getHashPassword() == passHash) {
+            return client;
+        } else {
+            throw new RuntimeException("Authentication fail");
+        }
+    }
+}
---
+++ HW4/HW/Main.java
@@ -0,0 +1,11 @@
+package HW.HW4.HW;
+
+import HW.HW4.HW.ClientApplication.Start;
+import HW.HW4.HW.Services.CashRepository;
+
+public class Main {
+    public static void main(String[] args) {
+        Start start = new Start();
+        start.runLoginRegisterMenu();;
+    }
+}
